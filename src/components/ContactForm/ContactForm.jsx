9b3f5ab0a71c7f1de5b11a79fef248c820ee7808
sivlvy <vladislavsidenko@ukr.net>
2024-01-07 17:50:29 +0200

    update

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,38 @@
+import css from './ContactForm.module.css';
+import { Component } from 'react';
+
+class ContactForm extends Component {
+	render() {
+		return (
+			<form className={css.form} onSubmit={this.handleSubmit}>
+				<label htmlFor="name" className={css.label}>
+					Name
+					<input
+						className={css.input}
+						type="text"
+						name="name"
+						id="name"
+						required
+						onChange={this.handleChange}
+					/>
+				</label>
+				<label htmlFor="number" className={css.label}>
+					Number
+					<input
+						className={css.input}
+						type="tel"
+						name="number"
+						id="number"
+						required
+						onChange={this.handleChange}
+					/>
+				</label>
+				<button className={css.btn} type="submit">
+					Add contact
+				</button>
+			</form>
+		);
+	}
+}
+
+export default ContactForm;
