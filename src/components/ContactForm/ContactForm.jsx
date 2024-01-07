ef90a8c618b44259f9bbe2e5220d9b263c762bda
sivlvy <vladislavsidenko@ukr.net>
2024-01-07 18:12:21 +0200

    update

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,6 +2,22 @@ import css from './ContactForm.module.css';
 import { Component } from 'react';
 
 class ContactForm extends Component {
+
+	state = {
+		name: '',
+		number: '',
+	}
+
+	handleChange = ({target: {name, value}}) => {
+		this.setState({[name]: value})
+	}
+
+	handleSubmit = e => {
+		e.preventDefault();
+		this.props.addContact({...this.state})
+		e.target.reset()
+	}
+
 	render() {
 		return (
 			<form className={css.form} onSubmit={this.handleSubmit}>
